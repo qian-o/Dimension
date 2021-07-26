0b7c676a153324e5f815d982267ccc0b4625bb0e
王 熙 <1324771795@qq.com>
2021-07-26 15:28:57 +0800

    图片切换

---
+++ DimensionClient/Library/Controls/DynamicImage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using DimensionClient.Common;
+using DimensionClient.Models.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DimensionClient.Library.Controls
+{
+    /// <summary>
+    /// DynamicImage.xaml 的交互逻辑
+    /// </summary>
+    public partial class DynamicImage : UserControl
+    {
+        public ImageSource ImagePath
+        {
+            get { return (ImageSource)GetValue(ImagePathProperty); }
+            set { SetValue(ImagePathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImagePath.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImagePathProperty =
+            DependencyProperty.Register("ImagePath", typeof(ImageSource), typeof(DynamicImage), new PropertyMetadata(null, ImagePathChanged));
+
+        public static void ImagePathChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue != null)
+            {
+                DynamicImage dynamic = d as DynamicImage;
+                DynamicImageViewModel data = ((Grid)dynamic.Content).DataContext as DynamicImageViewModel;
+                ImageSource image = e.NewValue as ImageSource;
+                if (!data.Show1)
+                {
+                    data.Source1 = image;
+                    if (!image.IsFrozen)
+                    {
+                        image.Changed += (a, b) =>
+                        {
+                            if (((ImageSource)a).ToString(ClassHelper.cultureInfo) == dynamic.ImagePath.ToString(ClassHelper.cultureInfo))
+                            {
+                                data.Show1 = true;
+                                data.Show2 = false;
+                            }
+                        };
+                    }
+                    else
+                    {
+                        data.Show1 = true;
+                        data.Show2 = false;
+                    }
+                }
+                else
+                {
+                    data.Source2 = image;
+                    if (!image.IsFrozen)
+                    {
+                        image.Changed += (a, b) =>
+                        {
+                            if (((ImageSource)a).ToString(ClassHelper.cultureInfo) == dynamic.ImagePath.ToString(ClassHelper.cultureInfo))
+                            {
+                                data.Show1 = false;
+                                data.Show2 = true;
+                            }
+                        };
+                    }
+                    else
+                    {
+                        data.Show1 = false;
+                        data.Show2 = true;
+                    }
+                }
+            }
+        }
+
+        public DynamicImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DimensionClient/Models/ViewModels/DynamicImageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using DimensionClient.Common;
+using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DimensionClient.Models.ViewModels
+{
+    public class DynamicImageViewModel : ModelBase
+    {
+        private bool show1;
+        private bool show2;
+        private ImageSource source1;
+        private ImageSource source2;
+
+        public bool Show1
+        {
+            get => show1;
+            set
+            {
+                show1 = value;
+                OnPropertyChanged(nameof(Show1));
+            }
+        }
+        public bool Show2
+        {
+            get => show2;
+            set
+            {
+                show2 = value;
+                OnPropertyChanged(nameof(Show2));
+            }
+        }
+        public ImageSource Source1
+        {
+            get => source1;
+            set
+            {
+                source1 = value;
+                OnPropertyChanged(nameof(Source1));
+            }
+        }
+        public ImageSource Source2
+        {
+            get => source2;
+            set
+            {
+                source2 = value;
+                OnPropertyChanged(nameof(Source2));
+            }
+        }
+
+        public override void InitializeVariable()
+        {
+            show1 = false;
+            show2 = true;
+            source1 = new BitmapImage(new Uri("/Library/Image/图片.png", UriKind.Relative));
+            source2 = new BitmapImage(new Uri("/Library/Image/图片.png", UriKind.Relative));
+        }
+    }
+}
