655ad8fcaf192f05ba636ea47bf04d9948345ab6
熙 王 <1324771795@qq.com>
2021-08-22 00:30:52 +0800

    提交

---
+++ DimensionService/Common/DbContextHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DimensionService.Common
+{
+    public static class DbContextHelper
+    {
+        public static DbSet<T> GetTable<T>(this DbContext context, string name) where T : class
+        {
+            return context.GetType().GetProperty(name).GetValue(context) as DbSet<T>;
+        }
+    }
+}
