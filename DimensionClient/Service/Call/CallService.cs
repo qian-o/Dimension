f2e8ef9792e15023ec7023cb41379c1b2999ad7d
熙 王 <1324771795@qq.com>
2021-10-28 19:11:45 +0800

    无

--- DimensionClient/Service/Call/CallService.cs
+++ DimensionClient/Service/Call/CallService.cs
@@ -2,11 +2,7 @@
 using DimensionClient.Models.ResultModels;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DimensionClient.Service.Call
 {
--- DimensionService/Common/TLSSigAPIv2.cs
+++ DimensionService/Common/TLSSigAPIv2.cs
@@ -1,12 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
 using Org.BouncyCastle.Utilities.Zlib;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DimensionService.Common
 {
