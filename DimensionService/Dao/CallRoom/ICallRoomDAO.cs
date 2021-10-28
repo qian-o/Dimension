bc8c2d975ae4cdd724110017f890391250e0466c
熙 王 <1324771795@qq.com>
2021-10-28 13:51:04 +0800

    更新

--- DimensionService/Dao/CallRoom/ICallRoomDAO.cs
+++ DimensionService/Dao/CallRoom/ICallRoomDAO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DimensionService.Dao.CallRoom
+﻿namespace DimensionService.Dao.CallRoom
 {
     public interface ICallRoomDAO
     {
--- DimensionService/Service/Call/ICallService.cs
+++ DimensionService/Service/Call/ICallService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DimensionService.Service.Call
+﻿namespace DimensionService.Service.Call
 {
     public interface ICallService
     {
