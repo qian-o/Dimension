75c62f59aaba31fe80462f864a46306987e44082
熙 王 <1324771795@qq.com>
2021-12-10 15:40:50 +0800

    新增指针附加事件，按下和抬起。

---
+++ DimensionClient/Common/PointerEventHelper.cs
@@ -0,0 +1,67 @@
+﻿using DimensionClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace DimensionClient.Common
+{
+    public static class PointerEventHelper
+    {
+        private static readonly List<PointerEventModel> pointerUpEvents = new();
+        private static readonly List<PointerEventModel> pointerDownEvents = new();
+
+        #region PointerUp
+        public static readonly RoutedEvent PointerUpEvent = EventManager.RegisterRoutedEvent("PointerUp", RoutingStrategy.Direct, typeof(EventHandler), typeof(UIElement));
+
+        public static void AddPointerUpHandler(this UIElement element, EventHandler handler)
+        {
+            lock (pointerUpEvents)
+            {
+                PointerEventModel pointer = PointerEventModel.Instance(handler);
+                pointerUpEvents.Add(pointer);
+                element.MouseLeftButtonUp += pointer.MouseButtonEvent;
+                element.TouchUp += pointer.TouchEvent;
+            }
+        }
+        public static void RemovePointerUpHandler(this UIElement element, EventHandler handler)
+        {
+            lock (pointerUpEvents)
+            {
+                if (pointerUpEvents.Find(item => item.EventHandler == handler) is PointerEventModel pointer)
+                {
+                    element.MouseLeftButtonUp -= pointer.MouseButtonEvent;
+                    element.TouchUp -= pointer.TouchEvent;
+                    pointerUpEvents.Remove(pointer);
+                }
+            }
+        }
+        #endregion
+
+        #region PointerDown
+        public static readonly RoutedEvent PointerDownEvent = EventManager.RegisterRoutedEvent("PointerDown", RoutingStrategy.Direct, typeof(EventHandler), typeof(UIElement));
+
+        public static void AddPointerDownHandler(this UIElement element, EventHandler handler)
+        {
+            lock (pointerDownEvents)
+            {
+                PointerEventModel pointer = PointerEventModel.Instance(handler);
+                pointerDownEvents.Add(pointer);
+                element.MouseLeftButtonDown += pointer.MouseButtonEvent;
+                element.TouchDown += pointer.TouchEvent;
+            }
+        }
+        public static void RemovePointerDownHandler(this UIElement element, EventHandler handler)
+        {
+            lock (pointerDownEvents)
+            {
+                if (pointerDownEvents.Find(item => item.EventHandler == handler) is PointerEventModel pointer)
+                {
+                    element.MouseLeftButtonDown -= pointer.MouseButtonEvent;
+                    element.TouchDown -= pointer.TouchEvent;
+                    pointerDownEvents.Remove(pointer);
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ DimensionClient/Models/PointerEventModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DimensionClient.Models
+{
+    public class PointerEventModel
+    {
+        public EventHandler EventHandler { get; set; }
+        public MouseButtonEventHandler MouseButtonEvent { get; set; }
+        public EventHandler<TouchEventArgs> TouchEvent { get; set; }
+
+        public static PointerEventModel Instance(EventHandler handler)
+        {
+            return new PointerEventModel
+            {
+                EventHandler = handler,
+                MouseButtonEvent = (object sender, MouseButtonEventArgs e) =>
+                {
+                    if (sender is UIElement element)
+                    {
+                        if (e.StylusDevice == null)
+                        {
+                            handler.Invoke(sender, e);
+                        }
+                    }
+                },
+                TouchEvent = (object sender, TouchEventArgs e) =>
+                {
+                    handler.Invoke(sender, e);
+                }
+            };
+        }
+    }
+}
